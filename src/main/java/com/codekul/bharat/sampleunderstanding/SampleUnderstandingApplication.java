a7b9ee2a0c2e02e79ef0c586b95a1d53d7809888
aniruddha <aniruddha.kudalkar@gmail.com>
2018-10-02 15:13:01 +0530

    initial commit

---
+++ src/main/java/com/codekul/bharat/sampleunderstanding/SampleUnderstandingApplication.java
@@ -0,0 +1,12 @@
+package com.codekul.bharat.sampleunderstanding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleUnderstandingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleUnderstandingApplication.class, args);
+	}
+}
